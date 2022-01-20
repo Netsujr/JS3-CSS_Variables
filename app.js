28f1763f59a7949f575745fb0a2ff09994f74910
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-20 11:12:21 +0900

    understanding data sets

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const inputs = document.querySelectorAll('.controls input');
 // querySelectorAll returns an node list not an array
 function handleUpdate() {
-console.log(this.value);
+// console.log(this.value);
+const suffix = this.dataset.sizing; // targeting sizing, if you have more than one data attribute
+// takes everything that has 'data-', puts it into an object
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
