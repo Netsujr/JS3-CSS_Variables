3354ca941f2d502324c4bfb5490af4e1170463e7
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-20 11:17:39 +0900

    challange done
    understanding this.####
    captured suffix from data attribute
    added suffix at the end of what needed to be added

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const inputs = document.querySelectorAll('.controls input');
 // querySelectorAll returns an node list not an array
 function handleUpdate() {
-// console.log(this.value);
-const suffix = this.dataset.sizing; // targeting sizing, if you have more than one data attribute
-// takes everything that has 'data-', puts it into an object
+  // console.log(this.value);
+  const suffix = this.dataset.sizing || ''; // targeting sizing, if you have more than one data attribute
+  // takes everything that has 'data-', puts it into an object
+  // console.log(suffix);
+  // console.log(this.name);
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
